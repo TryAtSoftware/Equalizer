79a77744572b1d2addd483aa18f68751950866ad
Tony Troeff <tonyslav_troeff@outlook.com>
2022-12-03 23:58:40 +0200

    Added tests for the differentiation rule constructor.

---
+++ TryAtSoftware.Equalizer.Core.Tests/Profiles/Complex/Rules/DifferentiationRuleTests.cs
@@ -0,0 +1,15 @@
+﻿namespace TryAtSoftware.Equalizer.Core.Tests.Profiles.Complex.Rules;
+
+using System;
+using TryAtSoftware.Equalizer.Core.Profiles.Complex.Rules;
+using Xunit;
+
+public class DifferentiationRuleTests
+{
+    [Fact]
+    public void DifferentiationRuleShouldNotBeInstantiatedWithInvalidValues()
+    {
+        Assert.Throws<ArgumentNullException>(() => new DifferentiationRule<int, int>(x => x, null!));
+        Assert.Throws<ArgumentNullException>(() => new DifferentiationRule<int, int>(null!, x => x));
+    }
+}
