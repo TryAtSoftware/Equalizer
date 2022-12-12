7ec6b694274cb99b4a3838b2935794769d9c6134
Tony Troeff <tonyslav_troeff@outlook.com>
2022-12-12 23:50:29 +0200

    Added some basic tests that should be used to validate the general equalization profile's behavior.

---
+++ TryAtSoftware.Equalizer.Core.Tests/Profiles/General/GeneralEqualizationProfileTests.cs
@@ -0,0 +1,42 @@
+﻿namespace TryAtSoftware.Equalizer.Core.Tests.Profiles.General;
+
+using TryAtSoftware.Equalizer.Core.Assertions;
+using TryAtSoftware.Equalizer.Core.ProfileProviders;
+using TryAtSoftware.Equalizer.Core.Profiles.General;
+using TryAtSoftware.Equalizer.Core.Tests.Models.Shopping;
+using TryAtSoftware.Equalizer.Core.Tests.Randomization.Shopping;
+using Xunit;
+
+public static class GeneralEqualizationProfileTests
+{
+    [Fact]
+    public static void GeneralEqualityShouldBeSuccessfullyValidated()
+    {
+        var shopRandomizer = new ShopRandomizer();
+        var shop1 = shopRandomizer.PrepareRandomValue();
+        var shop2 = shop1.Duplicate();
+
+        var equalizer = PrepareEqualizer();
+        equalizer.AssertEquality(shop1, shop2);
+    }
+    
+    [Fact]
+    public static void GeneralInequalityShouldBeSuccessfullyValidated()
+    {
+        var shopRandomizer = new ShopRandomizer();
+        var shop1 = shopRandomizer.PrepareRandomValue();
+        var shop2 = shopRandomizer.PrepareRandomValue();
+
+        var equalizer = PrepareEqualizer();
+        Assert.Throws<InvalidAssertException>(() => equalizer.AssertEquality(shop1, shop2));
+    }
+
+    private static Equalizer PrepareEqualizer()
+    {
+        var equalizer = new Equalizer();
+        var profilesProvider = new DedicatedProfileProvider();
+        profilesProvider.AddProfile(new GeneralEqualizationProfile<Shop>());
+        equalizer.AddProfileProvider(profilesProvider);
+        return equalizer;
+    }
+}
