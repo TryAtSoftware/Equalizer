c8ac8aabcda1c6b06b2b79e9d4f8e8af5d03fed3
Tony Troeff <tonyslav_troeff@outlook.com>
2022-12-19 16:15:07 +0200

    Made the general equalization context reusable and interchangeable for all projects using the Equalizer library.

--- TryAtSoftware.Equalizer.Core/Profiles/General/GeneralEqualizationProfile.cs
+++ TryAtSoftware.Equalizer.Core/Profiles/General/GeneralEqualizationProfile.cs
@@ -1,14 +1,23 @@
 ﻿namespace TryAtSoftware.Equalizer.Core.Profiles.General;
 
+using System.Linq;
+using JetBrains.Annotations;
 using TryAtSoftware.Equalizer.Core.Extensions;
 using TryAtSoftware.Equalizer.Core.Interfaces;
 using TryAtSoftware.Extensions.Collections;
 
 public class GeneralEqualizationProfile<T> : BaseTypedEqualizationProfile<T, T>
 {
+    [NotNull] private readonly IGeneralEqualizationContext<T> _generalEqualizationContext;
+
+    public GeneralEqualizationProfile([CanBeNull] IGeneralEqualizationContext<T> generalEqualizationContext = null)
+    {
+        this._generalEqualizationContext = generalEqualizationContext ?? GeneralEqualizationContext<T>.Instance;
+    }
+
     public override IEqualizationResult Equalize(T expected, T actual, IEqualizationOptions options)
     {
-        foreach (var (memberName, valueSelector) in GeneralEqualizationMembersCache<T>.ValueAccessors.OrEmptyIfNull())
+        foreach (var (memberName, valueSelector) in this._generalEqualizationContext.ValueAccessors.OrEmptyIfNull().Where(x => x.Value is not null))
         {
             var expectedValue = valueSelector(expected);
             var actualValue = valueSelector(actual);
