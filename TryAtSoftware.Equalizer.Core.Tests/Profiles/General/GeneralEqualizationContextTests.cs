a3fdf7a5232a7f571809ebbc0cff8d3c9c2c4b89
Tony Troeff <tonyslav_troeff@outlook.com>
2022-12-19 16:21:57 +0200

    Added tests for the general equalization context instantiation.

---
+++ TryAtSoftware.Equalizer.Core.Tests/Profiles/General/GeneralEqualizationContextTests.cs
@@ -0,0 +1,21 @@
+﻿namespace TryAtSoftware.Equalizer.Core.Tests.Profiles.General;
+
+using System;
+using System.Reflection;
+using TryAtSoftware.Equalizer.Core.Profiles.General;
+using TryAtSoftware.Equalizer.Core.Tests.Models.Shopping;
+using TryAtSoftware.Extensions.Reflection;
+using Xunit;
+
+public class GeneralEqualizationContextTests
+{
+    [Fact]
+    public void GeneralEqualizationContextMustNotBeInstantiatedWithoutMembersBinder() => Assert.Throws<ArgumentNullException>(() => new GeneralEqualizationContext<Shop>(null));
+
+    [Fact]
+    public void GeneralEqualizationContextMustNotBeInstantiatedWithInvalidMembersBinder()
+    {
+        var invalidMembersBinder = new MembersBinder<Product>(isValid: null, BindingFlags.Public | BindingFlags.Public);
+        Assert.Throws<InvalidOperationException>(() => new GeneralEqualizationContext<Shop>(invalidMembersBinder));
+    }
+}
