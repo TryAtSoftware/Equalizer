a204c2b46db59f414c9c6597b848a054f2deb877
Tony Troeff <tonyslav_troeff@outlook.com>
2022-12-23 00:48:44 +0200

    Removed the `JetBrains.Annotations` dependency

--- TryAtSoftware.Equalizer.Core/ProfileProviders/DynamicProfileProvider.cs
+++ TryAtSoftware.Equalizer.Core/ProfileProviders/DynamicProfileProvider.cs
@@ -2,7 +2,6 @@
 
 using System;
 using System.Collections.Generic;
-using JetBrains.Annotations;
 using TryAtSoftware.Equalizer.Core.Interfaces;
 
 public class DynamicProfileProvider : IEqualizationProfileProvider
