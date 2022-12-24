12f871193f7fcec7b0844ce70183a8bad1dc0c6d
Tony Troeff <tonyslav_troeff@outlook.com>
2022-12-24 15:19:34 +0200

    Wrote documentation for all value templates

--- TryAtSoftware.Equalizer.Core/Templates/GreaterThanOrEqualValueTemplate.cs
+++ TryAtSoftware.Equalizer.Core/Templates/GreaterThanOrEqualValueTemplate.cs
@@ -2,14 +2,26 @@
 
 using TryAtSoftware.Equalizer.Core.Extensions;
 
+/// <summary>
+/// A "value template" interconnected with the `greater than or equal to a value` logical function.
+/// </summary>
+/// <remarks>In order to use this value template, the actual value should be implementing the <see cref="System.IComparable"/> interface.</remarks>
 public class GreaterThanOrEqualValueTemplate
 {
+    /// <summary>
+    /// Gets the principal value for the subsequently executed comparison. The equality will be validated only if the `actual` value is greater than or equal to this one.
+    /// </summary>
     public object? Value { get; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GreaterThanOrEqualValueTemplate"/> class.
+    /// </summary>
+    /// <param name="value">The value that should be set to the <see cref="Value"/> property.</param>
     public GreaterThanOrEqualValueTemplate(object? value)
     {
         this.Value = value;
     }
 
+    /// <inheritdoc />
     public override string ToString() => $">= {this.Value.ToNormalizedString()}";
 }
