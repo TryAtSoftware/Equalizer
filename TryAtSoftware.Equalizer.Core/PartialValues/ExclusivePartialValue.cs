aba39bd802697f31aee142af83f0160900914db2
Tony Troeff <tonyslav_troeff@outlook.com>
2023-02-03 18:21:16 +0200

    Made changes to the PArtial general equalization so that it can be used with polymorphic hierarchies

--- TryAtSoftware.Equalizer.Core/PartialValues/ExclusivePartialValue.cs
+++ TryAtSoftware.Equalizer.Core/PartialValues/ExclusivePartialValue.cs
@@ -9,7 +9,7 @@ using TryAtSoftware.Extensions.Collections;
 /// An implementation <see cref="IPartialValue{T}"/> interface that includes all members but a predefined set of excluded ones.
 /// </summary>
 /// <typeparam name="T">The type of the represented value.</typeparam>
-public class ExclusivePartialValue<T> : IPartialValue<T>
+public class ExclusivePartialValue<T> : BasePartialValue<T>
     where T : notnull
 {
     private readonly HashSet<string> _membersToExclude;
@@ -17,18 +17,15 @@ public class ExclusivePartialValue<T> : IPartialValue<T>
     /// <summary>
     /// Initializes a new instance of the <see cref="ExclusivePartialValue{T}"/> class.
     /// </summary>
-    /// <param name="value">The value that should be set to the <see cref="Value"/> property.</param>
+    /// <param name="value">The value that should be set to the <see cref="IPartialValue{T}.Value"/> property.</param>
     /// <param name="membersToExclude">A collection of member keys that should not be included within this representation.</param>
     /// <exception cref="ArgumentNullException">Thrown if the <paramref name="value"/> is null.</exception>
     public ExclusivePartialValue(T value, params string[] membersToExclude)
+        : base(value)
     {
-        this.Value = value ?? throw new ArgumentNullException(nameof(value));
         this._membersToExclude = membersToExclude.OrEmptyIfNull().IgnoreNullValues().ToHashSet(StringComparer.OrdinalIgnoreCase);
     }
 
     /// <inheritdoc />
-    public T Value { get; }
-
-    /// <inheritdoc />
-    public bool IncludesMember(string memberKey) => !this._membersToExclude.Contains(memberKey);
+    public override bool IncludesMember(string memberKey) => !this._membersToExclude.Contains(memberKey);
 }
--- TryAtSoftware.Equalizer.Core/PartialValues/UnlimitedPartialValue.cs
+++ TryAtSoftware.Equalizer.Core/PartialValues/UnlimitedPartialValue.cs
@@ -6,22 +6,19 @@ using System;
 /// An implementation <see cref="IPartialValue{T}"/> interface that includes all members by default.
 /// </summary>
 /// <typeparam name="T">The type of the represented value.</typeparam>
-public class UnlimitedPartialValue<T> : IPartialValue<T>
+public class UnlimitedPartialValue<T> : BasePartialValue<T>
     where T : notnull
 {
     /// <summary>
     /// Initializes a new instance of the <see cref="UnlimitedPartialValue{T}"/> class.
     /// </summary>
-    /// <param name="value">The value that should be assigned to the <see cref="Value"/> property.</param>
+    /// <param name="value">The value that should be assigned to the <see cref="IPartialValue{T}.Value"/> property.</param>
     /// <exception cref="ArgumentNullException">Thrown if the <paramref name="value"/> is null.</exception>
     public UnlimitedPartialValue(T value)
+        : base(value)
     {
-        this.Value = value ?? throw new ArgumentNullException(nameof(value));
     }
 
     /// <inheritdoc />
-    public T Value { get; }
-
-    /// <inheritdoc />
-    public bool IncludesMember(string memberKey) => true;
+    public override bool IncludesMember(string memberKey) => true;
 }
