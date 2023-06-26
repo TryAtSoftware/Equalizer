f396ee6c5792b9813e888a309dd8ff5c38fe48be
Tony Troeff <tonyslav_troeff@outlook.com>
2023-06-27 00:35:39 +0300

    Extended tests so that they cover the obsolete `Extend` method as well.

--- TryAtSoftware.Equalizer.Core.Tests/Profiles/CodeRepositoryEqualizationProfile.cs
+++ TryAtSoftware.Equalizer.Core.Tests/Profiles/CodeRepositoryEqualizationProfile.cs
@@ -9,13 +9,30 @@ public class CodeRepositoryEqualizationProfile : ComplexEqualizationProfile<Code
     public CodeRepositoryEqualizationProfile()
     {
         this.Extend(new CommonIdentifiableEqualizationProfile<int>());
-        this.Equalize(rp => rp.Name, r => r.Name);
+        this.Extend(new CodeRepositoryEqualizationProfilePart1());
+        this.Extend(new CodeRepositoryEqualizationProfilePart2());
+
         this.Equalize(rp => rp.Description, r => r.Description);
         this.Equalize(5, r => r.OrganizationId);
-        this.Differentiate(rp => rp.Name, r => r.InternalName);
-        this.Differentiate(Value.Empty, r => r.InternalName);
         this.Equalize(rp => rp.CommitMessages, r => r.InitialCommits);
         this.Equalize(Value.Empty, r => r.SubsequentCommits);
+    }
+}
+
+file class CodeRepositoryEqualizationProfilePart1 : ComplexEqualizationProfile<CodeRepositoryPrototype, CodeRepository>
+{
+    public CodeRepositoryEqualizationProfilePart1()
+    {
+        this.Equalize(rp => rp.Name, r => r.Name);
+        this.Differentiate(rp => rp.Name, r => r.InternalName);
+        this.Differentiate(Value.Empty, r => r.InternalName);
+    }
+}
+
+file class CodeRepositoryEqualizationProfilePart2 : ComplexEqualizationProfile<CodeRepositoryPrototype, CodeRepository>
+{
+    public CodeRepositoryEqualizationProfilePart2()
+    {
         this.Equalize(Value.LowerThan(100), r => r.Likes);
         this.Equalize(Value.GreaterThanOrEqual(3), r => r.Likes);
     }
