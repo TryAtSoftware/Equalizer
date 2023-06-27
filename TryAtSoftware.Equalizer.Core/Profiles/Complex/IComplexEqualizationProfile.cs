e684798291363a7fe419c55e7b41cc5b9b51e400
Tony Troeff <tonyslav_troeff@outlook.com>
2023-06-27 14:57:38 +0300

    Improved documentation.

--- TryAtSoftware.Equalizer.Core/Profiles/Complex/IComplexEqualizationProfile.cs
+++ TryAtSoftware.Equalizer.Core/Profiles/Complex/IComplexEqualizationProfile.cs
@@ -3,7 +3,15 @@
 using System.Collections.Generic;
 using TryAtSoftware.Equalizer.Core.Profiles.Complex.Rules;
 
+/// <summary>
+/// An interface defining the structure of a component responsible for equalizing complex values.
+/// </summary>
+/// <typeparam name="TExpected">The type of the expected value.</typeparam>
+/// <typeparam name="TActual">The type of the actual value.</typeparam>
 public interface IComplexEqualizationProfile<in TExpected, in TActual>
 {
+    /// <summary>
+    /// Gets a read-only collection containing all registered <see cref="IComplexEqualizationRule{TExpected,TActual}"/> instances.
+    /// </summary>
     IReadOnlyCollection<IComplexEqualizationRule<TExpected, TActual>> Rules { get; }
 }
