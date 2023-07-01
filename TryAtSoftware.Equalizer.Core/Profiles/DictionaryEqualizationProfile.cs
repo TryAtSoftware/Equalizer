d93ce94734eff7faa2f27183ceb68b04c2360e23
Tony Troeff <tonyslav_troeff@outlook.com>
2023-07-01 22:41:43 +0300

    Proposed a simplistic implementation of an equalization profile for dictionaries.

---
+++ TryAtSoftware.Equalizer.Core/Profiles/DictionaryEqualizationProfile.cs
@@ -0,0 +1,36 @@
+﻿namespace TryAtSoftware.Equalizer.Core.Profiles;
+
+using System.Collections;
+using TryAtSoftware.Equalizer.Core.Extensions;
+using TryAtSoftware.Equalizer.Core.Interfaces;
+
+/// <summary>
+/// An implementation of the <see cref="IEqualizationProfile"/> interface responsible for equalizing two collections.
+/// </summary>
+public class DictionaryEqualizationProfile : BaseTypedEqualizationProfile<IDictionary, IDictionary>
+{
+    /// <inheritdoc />
+    protected override IEqualizationResult Equalize(IDictionary expected, IDictionary actual, IEqualizationOptions options)
+    {
+        var countEqualizationResult = options.Equalize(expected.Count, actual.Count);
+        if (!countEqualizationResult.IsSuccessful)
+        {
+            var errorMessage = this.UnsuccessfulEqualization(expected, actual, "Counts do not match.");
+            return new UnsuccessfulEqualizationResult(errorMessage.WithInner(countEqualizationResult));
+        }
+
+        foreach (var key in expected.Keys)
+        {
+            if (!actual.Contains(key)) return new UnsuccessfulEqualizationResult(this.UnsuccessfulEqualization(expected, actual, $"An entry for key {key} does not exist."));
+
+            var equalizationResult = options.Equalize(expected[key], actual[key]);
+            if (!equalizationResult.IsSuccessful)
+            {
+                var errorMessage = this.UnsuccessfulEqualization(expected, actual, $"Values for key {key} do not match");
+                return new UnsuccessfulEqualizationResult(errorMessage.WithInner(equalizationResult));
+            }
+        }
+
+        return new SuccessfulEqualizationResult();
+    }
+}
